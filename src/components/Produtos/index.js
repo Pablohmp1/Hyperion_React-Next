4b2b44eb6526b904feacef04bdd2df457ed734e8
Pablo Pires <pablo.pires@aluno.ifsp.edu.br>
2025-08-14 17:24:40 -0300

    Adicionando os Produtos

---
+++ src/components/Produtos/index.js
@@ -0,0 +1,11 @@
+import styles from './Produtos.module.css'
+
+export default function Produtos() {
+    return (
+        <section id='produto' className={styles.produtos}>
+            <div className={styles.titulos}>
+                
+            </div>
+        </section>
+    )
+}
